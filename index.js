f8469fed3fbb616d1d11a0594293a53ddf29a818
Scott Corgan <scottcorgan@gmail.com>
2013-12-27 15:44:35 -0800

    Update domy-element to 0.3.x and refactor for wrap

--- index.js
+++ index.js
@@ -1,50 +1,47 @@
 var element = require('domy-element');
 
-module.exports = function insert (elem) {
-  var domElement = element(elem);
-  
-  return new Inserter(domElement);
+var insertMethods = {
+  before: function (parentElement, parent) {
+    this._adjacent(this.element, element(parentElement), parent);
+    
+    return this;
+  },
+
+  after: function (parentElement, parent) {
+    this._adjacent(this.element, element(parentElement).nextSibling, parent);
+    
+    return this;
+  },
+
+  beginning: function (parentElement) {
+    var parentDomEl = this._queryParent(parentElement);
+    this.before(parentDomEl.firstChild, parentDomEl);
+    
+    return this;
+  },
+
+  end: function (parentElement) {
+    this._queryParent(parentElement).appendChild(this.element);
+    
+    return this;
+  },
+
+  _adjacent: function (elem, adjacentTo, parent) {
+    var domEl = element(elem);
+    this._queryParent(parent).insertBefore(domEl, adjacentTo);
+    
+    return domEl;
+  },
+
+  _queryParent: function (parent) {
+    if (!parent) parent = document.body;
+    if (typeof parent === 'string') parent = document.querySelector(parent);
+    
+    return parent;
+  }
 };
 
-var Inserter = function (elem) {
-  this.element = elem;
+module.exports = function children (elem) {
+  return element.wrap(elem, insertMethods);
 };
 
-Inserter.prototype.before = function (parentElement, parent) {
-  this._adjacent(this.element, element(parentElement), parent);
-  
-  return this;
-};
-
-Inserter.prototype.after = function (parentElement, parent) {
-  this._adjacent(this.element, element(parentElement).nextSibling, parent);
-  
-  return this;
-};
-
-Inserter.prototype.beginning = function (parentElement) {
-  var parentDomEl = this._queryParent(parentElement);
-  this.before(parentDomEl.firstChild, parentDomEl);
-  
-  return this;
-};
-
-Inserter.prototype.end = function (parentElement) {
-  this._queryParent(parentElement).appendChild(this.element);
-  
-  return this;
-};
-
-Inserter.prototype._adjacent = function (elem, adjacentTo, parent) {
-  var domEl = element(elem);
-  this._queryParent(parent).insertBefore(domEl, adjacentTo);
-  
-  return domEl;
-}
-
-Inserter.prototype._queryParent = function (parent) {
-  if (!parent) parent = document.body;
-  if (typeof parent === 'string') parent = document.querySelector(parent);
-  
-  return parent;
-};
